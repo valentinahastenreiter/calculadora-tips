a10458a343408c300103b017693ce3bd842e4e8d
Frau Hastenreiter <valentina.hastenreiter@gmail.com>
2022-06-06 15:09:51 -0300

    primeiro commit calculadora de tips

---
+++ tip-calculator-app-main/calculadora-tips/script.js
@@ -0,0 +1,8 @@
+let bill = document.getElementById('valorConta');
+let tipCustom = document.getElementById('tipCustom');
+let numberPeople = document.getElementById('numberPeople')
+
+
+
+
+console.log('valorConta')
